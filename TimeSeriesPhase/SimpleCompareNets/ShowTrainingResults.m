44b98654058be51b4797bcae1f85aa0f33ca9da8
Daniel <dbauchspiess@gmail.com>
2021-03-24 15:39:42 -0300

    Teste e leitura do teste variando neurônios e epocas

--- TimeSeriesPhase/SimpleCompareNets/ShowTrainingResults.m
+++ TimeSeriesPhase/SimpleCompareNets/ShowTrainingResults.m
@@ -4,31 +4,49 @@
 %legend('Sinal mixado apenas', 'Sinais puros apenas', 'Sinais mixados e puros');
 %title('Tempo de treinamento pelo número de neurônios');
 
-%Monta vetores de training loss e os apresenta
-for i=2:50
-    lossMixed{i} = trMixed{i}.TrainingLoss(end);
-    lossPures{i} = trPures{i}.TrainingLoss(end);
-    lossAll{i} = trAll{i}.TrainingLoss(end);
+
+
+%Loss x neuronios, epocas constante
+epochs = 20;
+epochsI = ((epochs - 20)/100)+1;
+for i=2:10:52
+    lossMixed{i} = trMixed{epochsI}{i}.TrainingLoss(end);
+    lossPures{i} = trPures{epochsI}{i}.TrainingLoss(end);
+    lossAll{i} = trAll{epochsI}{i}.TrainingLoss(end);
 end
-%plot(2:50, cell2mat(lossMixed), 2:50, cell2mat(lossPures), 2:50, cell2mat(lossAll))
-%legend('Sinal mixado apenas', 'Sinais puros apenas', 'Sinais mixados e puros');
-%title('Loss de treino pelo número de neurônios');
-%xlabel('Neurônios');
-%ylabel('Loss');
+plot(2:10:52, cell2mat(lossMixed), 2:10:52, cell2mat(lossPures), 2:10:52, cell2mat(lossAll))
+legend('Sinal mixado apenas', 'Sinais puros apenas', 'Sinais mixados e puros');
+title("Loss de treino pelo número de neurônios, "+num2str(epochs)+" épocas");
+xlabel('Neurônios');
+ylabel('Loss');
+
+
+%Loss x epocas, neuronios constantes
+neuronios = 2;
+for i=20:100:1020
+    lossMixed{i} = trMixed{i}{neuronios}.TrainingLoss(end);
+    lossPures{i} = trPures{i}{neuronios}.TrainingLoss(end);
+    lossAll{i} = trAll{i}{neuronios}.TrainingLoss(end);
+end
+plot(2:10:52, cell2mat(lossMixed), 2:10:52, cell2mat(lossPures), 2:10:52, cell2mat(lossAll))
+legend('Sinal mixado apenas', 'Sinais puros apenas', 'Sinais mixados e puros');
+title("Loss de treino pelo número de épocas, "+num2str(neuronios)+" neuronios");
+xlabel('MaxEpochs');
+ylabel('Loss');
 
 
 
 %Monta vetores de training RMSE e os apresenta
-for i=2:50
-    rmseMixed{i} = trMixed{i}.TrainingRMSE(end);
-    rmsePures{i} = trPures{i}.TrainingRMSE(end);
-    rmseAll{i} = trAll{i}.TrainingRMSE(end);
-end
-plot(2:50, cell2mat(rmseMixed), 2:50, cell2mat(rmsePures), 2:50, cell2mat(rmseAll))
-legend('Sinal mixado apenas', 'Sinais puros apenas', 'Sinais mixados e puros');
-title('RMSE de treino pelo número de neurônios');
-xlabel('Neurônios');
-ylabel('RMS');
+%for i=2:10:52
+%    rmseMixed{i} = trMixed{i}.TrainingRMSE(end);
+%    rmsePures{i} = trPures{i}.TrainingRMSE(end);
+%    rmseAll{i} = trAll{i}.TrainingRMSE(end);
+%end
+%plot(2:50, cell2mat(rmseMixed), 2:50, cell2mat(rmsePures), 2:50, cell2mat(rmseAll))
+%legend('Sinal mixado apenas', 'Sinais puros apenas', 'Sinais mixados e puros');
+%title('RMSE de treino pelo número de neurônios');
+%xlabel('Neurônios');
+%ylabel('RMS');
 
 %Insere a entrada na rede novamente e recupera o RMSE para este caso
 
