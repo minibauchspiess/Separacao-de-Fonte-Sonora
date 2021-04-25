0574515f60266a8b9de530b1949fb7f48b76fc13
rodrigo.xavier <rodrigomatiasxavier@gmail.com>
2021-04-25 16:29:22 -0300

    InitialTests

--- DescriptorsPhase/Elman/InputMatHandler.m
+++
@@ -1,9 +0,0 @@
-function [X, T] = InputMatHandler(samplesFolder, cbFolder, outCb, flFolder, outFl, winSize, seqSize, k)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-[CbMats, cbTargets] = FolderMatrix(fullfile(samplesFolder,cbFolder), outCb, winSize);
-[FlMats, flTargets] = FolderMatrix(fullfile(samplesFolder,flFolder), outFl, winSize);
-
-[X, T] = BuildKGroups({CbMats,FlMats}, {cbTargets,flTargets}, k, seqSize);
-end
-
