a3f6c5ce080e869130e53e108eea587dcb43ed04
Daniel Bauchspiess <dbauchspiess@gmail.com>
2021-04-29 16:37:11 -0300

    Atualizações de report para melhor legibilidade

--- TimeSeriesPhase/LSTM/Testes artificial/Teste especializado mesma nota/LSTM.m
+++ TimeSeriesPhase/LSTM/Testes artificial/Teste especializado mesma nota/LSTM.m
@@ -4,20 +4,21 @@
 [tri, ~] = audioread("../Ondas de entrada/Triangulares/tri400.wav");
 s=s';sq=sq';tri=tri';
 
-%Criacao das envoltorias aplicadas a cada
+addpath("../../../SSS_Eval");
+numNeurons = [40 70 100 130 160];
 
-numNeurons = [40 70 100 130 160 190 220 250 300];
-
-for i=6:size(numNeurons,2)
+for i=1:size(numNeurons,2)
     fprintf("Iniciando treino com "+num2str(numNeurons(i))+" neuronios\n");
-    [layer, opt] = NetParams_LSTM(numNeurons(i), 1);
-    [net{i}, tr{i}, time(i), epochs(i)] = Train_LSTM(layer, opt, s, sq, tri, "All", fs);
+    [layer, opt] = NetParams_LSTM(numNeurons(i), 5);
+    [net{i}, tr{i}, time(i), sdrEvol(i)] = Train_LSTM(layer, opt, s, sq, tri, "MixedOnly", fs);
 
-    [~, outMixed{i}] = predictAndUpdateState(net{i}, repmat((s+sq+tri)/3,[1 100]));
-    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outSin.wav", outMixed{i}(1,:), fs);
-    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outSquare.wav", outMixed{i}(2,:), fs);
-    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outTri.wav", outMixed{i}(2,:), fs);
+    [~, outMixed{i}] = predictAndUpdateState(net{i}, (s+sq+tri)/3);
+    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outSin.wav", repmat(outMixed{i}(1,:), [1 100]), fs);
+    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outSquare.wav", repmat(outMixed{i}(2,:), [1 100]), fs);
+    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outTri.wav", repmat(outMixed{i}(3,:), [1 100]), fs);
+    
+   [SDR{i}, SIR{i}, SAR{i}, perm{i}] = bss_eval_sources(outMixed{i}, [s/3;sq/3;tri/3]);
 end
-save Resultados/vars2.mat
+save Resultados/varsSDR1.mat
 
 
--- TimeSeriesPhase/LSTM/Testes artificial/Teste especializado notas diferentes/LSTM.m
+++ TimeSeriesPhase/LSTM/Testes artificial/Teste especializado notas diferentes/LSTM.m
@@ -4,19 +4,20 @@
 [tri, ~] = audioread("../Ondas de entrada/Triangulares/tri500.wav");
 s=s';sq=sq';tri=tri';
 
-%Criacao das envoltorias aplicadas a cada
-
+addpath("../../../SSS_Eval");
 numNeurons = [40 70 100 130 160];
 
 for i=1:size(numNeurons,2)
     fprintf("Iniciando treino com "+num2str(numNeurons(i))+" neuronios\n");
     [layer, opt] = NetParams_LSTM(numNeurons(i), 5);
-    [net{i}, tr{i}, time(i), epochs(i)] = Train_LSTM(layer, opt, s, sq, tri, "All", fs);
+    [net{i}, tr{i}, time(i), sdrEvol{i}] = Train_LSTM(layer, opt, s, sq, tri, "MixedOnly", fs);
 
-    [~, outMixed{i}] = predictAndUpdateState(net, (s+sq+tri)/3);
-    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outSin.wav", outMixed{i}(1,:), fs);
-    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outSquare.wav", outMixed{i}(2,:), fs);
-    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outTri.wav", outMixed{i}(2,:), fs);
+    [~, outMixed{i}] = predictAndUpdateState(net{i}, (s+sq+tri)/3);
+    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outSin.wav", repmat(outMixed{i}(1,:), [1 100]), fs);
+    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outSquare.wav", repmat(outMixed{i}(2,:), [1 100]), fs);
+    audiowrite("Resultados/ondas/"+num2str(numNeurons(i))+"neurons_outTri.wav", repmat(outMixed{i}(3,:), [1 100]), fs);
+        
+   [SDR{i}, SIR{i}, SAR{i}, perm{i}] = bss_eval_sources(outMixed{i}, [s/3;sq/3;tri/3]);
 end
 save Resultados/vars2.mat
 
