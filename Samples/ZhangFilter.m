4f068170151da21dfa5c9bb6e232ac9ce951d11e
Daniel Bauchspiess <dbauchspiess@gmail.com>
2021-05-21 17:35:36 -0300

    Adaptação do filtro para plotar resultado em função do tempo, não das amostras

--- Samples/ZhangFilter.m
+++ Samples/ZhangFilter.m
@@ -11,8 +11,8 @@ A(2)=0;
 A(winQtd+3)=0;
 A(winQtd+4)=0;
 for i = 3:winQtd+2
-    buffer = somOrig((winSize*(i-3)+1):winSize*(i-2));
-    A(i) = max(abs(buffer))*2;
+    buffer = somOrig((winSize*(i-3)+1):winSize*(i-2));  %Pega uma janela do audio
+    A(i) = max(abs(buffer))*2;                          %Computa a amplitude maxima da janela
 end
 
 %Calcula o limiar baseado nas amplitudes
@@ -49,9 +49,9 @@ end
 pointBegin = ((cStart-3)*winSize+1);
 somFinal = somOrig(pointBegin:(cEnd-2)*winSize);
 
-plot(somOrig);
+plot(0:1/fs:size(somOrig,1)/fs-1/fs,somOrig);
 hold on
-plot(((cStart-3)*winSize+1):(cEnd-2)*winSize, somFinal);
+plot(((cStart-3)*winSize+1)/fs-1/fs:1/fs:(cEnd-2)*winSize/fs-1/fs, somFinal);
 hold off
 
 
